a1774b5dcdc21f81e280d80ab73465da9fc16156
Cata001 <catalinateguiani@yahoo.com>
2020-02-01 10:39:06 +0000

    Finish

--- script.js
+++ script.js
@@ -3,18 +3,42 @@ const quoteDisplayElement = document.getElementById("quoteDisplay");
 const quoteInputElement = document.getElementById("quoteInput");
 const timerElement = document.getElementById("timer");
 
+quoteInputElement.addEventListener("input", () => {
+  const arrayQuote = quoteDisplayElement.querySelectorAll("span");
+  const arrayValue = quoteInputElement.value.split("");
+  let correct = true;
+  arrayQuote.forEach((characterSpan, index) => {
+    const character = arrayValue[index];
+    if (character == null) {
+      characterSpan.classList.remove("correct");
+      characterSpan.classList.remove("incorrect");
+      correct = false;
+    } else if (character === characterSpan.innerText) {
+      characterSpan.classList.add("correct");
+      characterSpan.classList.remove("incorrect");
+    } else {
+      characterSpan.classList.add("incorrect");
+      characterSpan.classList.remove("correct");
+      correct = false;
+    }
+  });
+
+  if (correct) renderNewQuote();
+});
+
 function getRandomQuote() {
   return fetch(RANDOM_QUOTE_API_URL)
     .then(response => response.json())
     .then(data => data.content);
 }
 
-async function getNextQuote() {
+async function renderNewQuote() {
   const quote = await getRandomQuote();
   // quoteDisplayElement.innerText = "";
   quoteDisplayElement.innerHTML = "";
   quote.split("").forEach(character => {
     const characterSpan = document.createElement("span");
+    // characterSpan.classList.add("incorrect");
     characterSpan.innerText = character;
     quoteDisplayElement.appendChild(characterSpan);
   });
@@ -35,4 +59,4 @@ function getTimerTime() {
   return Math.floor((new Date() - startTime) / 1000);
 }
 
-getNextQuote();
+renderNewQuote();
